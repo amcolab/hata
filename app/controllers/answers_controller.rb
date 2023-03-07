9d3517eb926677a12e972f696f92581cd49b3ed3
amcolab <amcolabdev@gmail.com>
2023-03-07 18:46:19 +0700

    get list answers, grading, ranking, move to next question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,10 @@
 class AnswersController < ApplicationController
+  def index
+    active_question = Question.active.first
+    @answers = active_question.answers.active.with_round(active_question.current_round)
+      .ordered_by_created_at
+  end
+
   def update
     current_answer = Answer.find_by(user_id: current_user.id, question_id: Question.active.first)
     @success = current_answer && current_answer.update(answer_params)
@@ -10,6 +16,32 @@ class AnswersController < ApplicationController
 
   def get_assessment_path
     render json: { redirect: assessment_path }
+  end
+
+  def aggregate_points
+    # points param format: {points: {answer_id1: point1, answer_id2: point2, answer_id3: point3}}
+    answer_ids = params[:points].keys
+    if Assessment.by_assessment_user_id(current_user.id).by_answer_id(answer_ids).exists?
+      @success = false
+      @message = '採点済みのお題を再度採点出来ません。'
+    else
+      params[:points].each do |answer_id, point|
+        answer = Answer.active.find_by_id(answer_id)
+        next unless answer
+        answer.update_point!(point, current_user)
+      end
+      @success = true
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def result
+    active_question = Question.active.first
+    @answers = active_question.answers.active.with_round(active_question.current_round)
+      .ordered_by_point.includes(:user)
   end
 
   private
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,9 +10,6 @@ class GamesController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      active_question = Question.find_or_create_active_question
-      active_question.update(time_start: Time.current) if active_question.time_start.nil?
-      @user.answers.create!(question: active_question, status: 'active', round: active_question.current_round)
       redirect_to answer_path
     else
       flash[:error] = { user_login: @user.errors.messages }
@@ -22,11 +19,23 @@ class GamesController < ApplicationController
 
   # Display answer the question screen
   def answer_round
-    @answer = current_user.answers.active.includes(:question)
+    active_question = Question.find_or_create_active_question
+    active_question.update(time_start: Time.current) if active_question.time_start.nil?
+    @answer = current_user.answers.create!(question: active_question, status: 'active', round: active_question.current_round)
   end
 
   # Display assessment screen
   def assessment_round
+  end
+
+  # Archived a question, after finish display result screen
+  def archived_question
+    active_question = Question.active.first
+    if active_question && params[:id].present? && active_question.id == params[:id].to_i
+      active_question.archived!
+      active_question.answers.active.update_all(status: 'archived')
+    end
+    redirect_to answer_round_path
   end
 
   private
